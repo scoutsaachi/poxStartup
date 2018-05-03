fd791344e59bd81c31c2cafb5ddfcf67b535932b
root <root@cs244-vm.c.cs244-austin-tommy.internal>
2018-05-03 22:22:43 +0000

    Use networkx library functions instead of YenKSP

--- pox/pox/ext/build_topology.py
+++ pox/pox/ext/build_topology.py
@@ -5,7 +5,8 @@ import pdb
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-
+import pickle
+from itertools import islice
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import CPULimitedHost
@@ -55,69 +56,129 @@ def simpleTest():
         net.pingAll()
         net.stop()
 
-def load_random_graph(file, n):
-	graph = DiGraph()
-	for i in range(n):
-	    graph.add_node(str(i))
-	with open(file) as f:
-	    for line in f:
-		if line.startswith("#"):
-		    continue
-		tokens = line.split()
-		source_node = tokens[0]
-		if int(source_node) >= n:
-		    raise "Source node greater than max"
-		for dest_node in tokens[1:]:
-		    graph.add_edge(str(source_node), str(dest_node), 1)
-		    graph.add_edge(str(dest_node), str(source_node), 1)
-	return graph
-
-def count_shortest_paths(digraph, n):
-	counts = {}
+#def load_random_graph_into_digraph(file, n):
+#	graph = DiGraph()
+#	for i in range(n):
+#	    graph.add_node(str(i))
+#	with open(file) as f:
+#	    for line in f:
+#		if line.startswith("#"):
+#		    continue
+#		tokens = line.split()
+#		source_node = tokens[0]
+#		if int(source_node) >= n:
+#		    raise "Source node greater than max"
+#		for dest_node in tokens[1:]:
+#		    graph.add_edge(str(source_node), str(dest_node), 1)
+#		    graph.add_edge(str(dest_node), str(source_node), 1)
+#	return graph
+#
+def compute_ecmp_paths(networkx_graph, n):
+	ecmp_paths = {}
 	for a in range(n):
 	    for b in range(a+1, n):
-		ksp = ksp_yen(digraph, str(a), str(b), 64)
-		minCost = ksp[0]['cost']
-		for i in range(len(ksp)):
-		    if i >= 8 and ksp[i]['cost'] > minCost:
-			break
-		    else:
-			prev_node  = None
-			for node in ksp[i]['path']:
-			    if not prev_node:
-				prev_node = node
-				continue
-			    else:
-				link = (str(prev_node), str(node))
-				rev_link = (str(node), str(prev_node))
-				if link not in counts:
-			            counts[link] = {"8-ksp": 0, "8-ecmp": 0, "64-ecmp": 0}
-				if rev_link not in counts:
-			            counts[rev_link] = {"8-ksp": 0, "8-ecmp": 0, "64-ecmp": 0}
-				if ksp[i]['cost'] == minCost and i < 8:
-				    counts[link]["8-ecmp"] += 1
-				    counts[rev_link]["8-ecmp"] += 1
-				if ksp[i]['cost'] == minCost:
-				    counts[link]["64-ecmp"] += 1
-				    counts[rev_link]["64-ecmp"] += 1
-				if i < 8:
-				    counts[link]["8-ksp"] += 1
-				    counts[rev_link]["8-ksp"] += 1
-				prev_node = node
-	return counts
-	
+		shortest_paths = networkx.all_shortest_paths(networkx_graph, source=str(a), target=str(b))
+		ecmp_paths[(str(a), str(b))] = [p for p in shortest_paths]
+	return ecmp_paths
 
-def assemble_histogram(counts):
+def compute_k_shortest_paths(networkx_graph, n, k=8):
+	all_ksp = {}
+	for a in range(n):
+	    for b in range(a+1, n):
+		ksp = list(islice(networkx.shortest_simple_paths(networkx_graph, source=str(a), \
+								target=str(b)), k))
+		all_ksp[(str(a), str(b))] = ksp
+	return all_ksp
+
+#def compute_k_shortest_paths(digraph, n, k=8):
+#	counts = {}
+#	for a in range(n):
+#	    for b in range(a+1, n):
+#		ksp = ksp_yen(digraph, str(a), str(b), k)
+#		minCost = ksp[0]['cost']
+#		for i in range(len(ksp)):
+#		    if i >= 8 and ksp[i]['cost'] > minCost:
+#			break
+#		    else:
+#			prev_node  = None
+#			for node in ksp[i]['path']:
+#			    if not prev_node:
+#				prev_node = node
+#				continue
+#			    else:
+#				link = (str(prev_node), str(node))
+#				rev_link = (str(node), str(prev_node))
+#				if link not in counts:
+#			            counts[link] = {"8-ksp": 0, "8-ecmp": 0, "64-ecmp": 0}
+#				if rev_link not in counts:
+#			            counts[rev_link] = {"8-ksp": 0, "8-ecmp": 0, "64-ecmp": 0}
+#				if ksp[i]['cost'] == minCost and i < 8:
+#				    counts[link]["8-ecmp"] += 1
+#				    counts[rev_link]["8-ecmp"] += 1
+#				if ksp[i]['cost'] == minCost:
+#				    counts[link]["64-ecmp"] += 1
+#				    counts[rev_link]["64-ecmp"] += 1
+#				if i < 8:
+#				    counts[link]["8-ksp"] += 1
+#				    counts[rev_link]["8-ksp"] += 1
+#				prev_node = node
+#	return counts
+#	
+#
+def get_ecmp_path_counts(ecmp_paths):
+	counts = {}
+	for _, value in ecmp_paths.iteritems():
+	    if len(value) > 64:
+		value = value[:64]
+	    for i in range(len(value)):
+		path = value[i]
+		prev_node = None
+		for node in path:
+		    if not prev_node:
+			prev_node = node
+			continue
+		    link = (str(prev_node), str(node))
+		    rev_link = (str(node), str(prev_node))
+		    if link not in counts:
+			counts[link] = {"8-ecmp": 0, "64-ecmp": 0}
+		    if rev_link not in counts:
+			counts[rev_link] = {"8-ecmp": 0, "64-ecmp": 0}
+		    if i < 8:
+			counts[link]["8-ecmp"] += 1
+		    counts[link]["64-ecmp"] += 1
+		    prev_node = node
+	return counts
+
+def get_ksp_counts(all_ksp):
+	counts = {}
+        for _, value in all_ksp.iteritems():
+            for path in value:
+                prev_node = None
+                for node in path:
+                    if not prev_node:
+                        prev_node = node
+                        continue
+                    link = (str(prev_node), str(node))
+                    rev_link = (str(node), str(prev_node))
+                    if link not in counts:
+                        counts[link] = 0
+                    if rev_link not in counts:
+                        counts[rev_link] = 0
+                    counts[link] += 1
+                    prev_node = node
+        return counts
+
+def assemble_histogram(ecmp_path_counts, ksp_counts, file_name):
 	ksp_distinct_paths_counts = []
 	ecmp_8_distinct_paths_counts = []
 	ecmp_64_distinct_paths_counts = []
 	
 
-	for _, value in sorted(counts.iteritems(), key=lambda (k,v): (v["8-ksp"],k)):
-	    ksp_distinct_paths_counts.append(value["8-ksp"])
-	for _, value in sorted(counts.iteritems(), key=lambda (k,v): (v["8-ecmp"],k)):
+	for _, value in sorted(ksp_counts.iteritems(), key=lambda (k,v): (v,k)):
+	    ksp_distinct_paths_counts.append(value)
+	for _, value in sorted(ecmp_path_counts.iteritems(), key=lambda (k,v): (v["8-ecmp"],k)):
 	    ecmp_8_distinct_paths_counts.append(value["8-ecmp"])
-	for _, value in sorted(counts.iteritems(), key=lambda (k,v): (v["64-ecmp"],k)):
+	for _, value in sorted(ecmp_path_counts.iteritems(), key=lambda (k,v): (v["64-ecmp"],k)):
 	    ecmp_64_distinct_paths_counts.append(value["64-ecmp"])
 
 	print ksp_distinct_paths_counts
@@ -131,8 +192,15 @@ def assemble_histogram(counts):
 	ax1.scatter(x, ecmp_8_distinct_paths_counts, c='r', marker='o', label="8-ECMP")
 	ax1.scatter(x, ecmp_64_distinct_paths_counts, c='g', marker='x', label="64-ECMP")
 	plt.legend(loc="upper left");
-	plt.savefig("plot.png")
+	plt.savefig("%s_plot.png" % file_name)
 	    
+def save_obj(obj, name):
+    with open('pickle_obj/'+ name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, 0)
+
+def load_obj(name ):
+    with open('pickle_obj/' + name + '.pkl', 'rb') as f:
+        return pickle.load(f)
 
 
 def main():
@@ -140,22 +208,43 @@ def main():
 	#topo = JellyFishTop()
 	#net = Mininet(topo=topo, host=CPULimitedHost, link = TCLink, controller=JELLYPOX)
 	#experiment(net)
-	d = 14
-	n = 245
-	rrg = networkx.random_regular_graph(d, n)
-	file_name = "rrg_large_" + str(d) + "_" + str(n)
+	#d = 14
+	#n = 245
+	#rrg = networkx.random_regular_graph(d, n)
+	#file_name = "rrg_large_" + str(d) + "_" + str(n)
 
 	#networkx.write_adjlist(rrg, file_name)
-	print "Loading random graph into digraph"
- 	digraph = load_random_graph(file_name, n)
-	print "Running KSP"
-	shortest_path_counts = count_shortest_paths(digraph, n)
-	print "Making the plot"
-	assemble_histogram(shortest_path_counts)
+	#print "Loading random graph into digraph"
+ 	#digraph = load_random_graph_into_digraph(file_name, n)
+	#print "Running KSP"
+	#shortest_path_counts = count_shortest_paths(digraph, n)
+	#print "Making the plot"
+	#assemble_histogram(shortest_path_counts)
 	
 #	setLogLevel("info")
 #	simpleTest()
-
+	reuse_old_result = True
+	ecmp_paths = {}
+	all_ksp = {}
+	file_name = "rrg_small_3_10"
+	if not reuse_old_result:
+		graph = networkx.read_adjlist(file_name)
+		print "ECMP paths"
+		ecmp_paths = compute_ecmp_paths(graph, 10)
+		pdb.set_trace()	
+		save_obj(ecmp_paths, "ecmp_paths_%s" % (file_name))
+		print "K shortest paths"
+		all_ksp = compute_k_shortest_paths(graph, 10)
+		save_obj(all_ksp, "ksp_%s" % (file_name))
+	else:
+		ecmp_paths = load_obj("ecmp_paths_%s" % (file_name))
+		all_ksp = load_obj("ksp_%s" % (file_name))
+	print "Assembling counts from paths"
+	ecmp_path_counts = get_ecmp_path_counts(ecmp_paths)
+	ksp_counts = get_ksp_counts(all_ksp)
+	print "Making the plot"
+	assemble_histogram(ecmp_path_counts=ecmp_path_counts, ksp_counts=ksp_counts, file_name=file_name)
+	
 if __name__ == "__main__":
 	main()
 
